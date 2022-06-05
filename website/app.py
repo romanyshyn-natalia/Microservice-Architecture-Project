1ec88715cc2097ba96fd0ac1d9b6500afaab3dd9
Daria Omelkina <odasha2001@gmail.com>
2022-06-06 02:26:39 +0300

    New pages, easier data input.

--- website/app.py
+++ website/app.py
@@ -3,33 +3,38 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # if success:
+    #     return redirect(url_for('login'))
+    # else:
+    #     return render_template('register_unsuccessful.html')
 
-@app.route("/register", methods=["GET","POST"])
-def register_page():
-    # TODO:
     return render_template('register.html')
 
 
 @app.route("/login")
-def login_page():
+def login():
+    # if success:
+    #     return redirect(url_for('home'))
+    # else:
+    #     return render_template('login_unsuccessful.html')
+
     return render_template('login.html')
 
 
-@app.route("/")
-def home_page():
+@app.route("/home")
+def home():
     # Search patients: GET (name, surname) (id) -> data
     # Search doctors: GET (name, surname) (id) -> data
-    return ""
+    return render_template('home.html')
 
 
-@app.route("/patients")
-def patients_page():
-    return ""
-
-
-@app.route("/doctors")
-def doctors_page():
-    return ""
+@app.route("/results")
+def results():
+    return render_template('results.html', results=[
+        {"name": "John", "surname": "Smith", "id": "12345", "status": "Rehabilitation", "diagnosis": "Unknown",
+         "date": "12.04.2010", "age": "20", "sex": "Male", "doctor": "Tom Brown (5123)"}])
 
 
 if __name__ == "__main__":
