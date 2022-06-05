48f64fc4c9965781295336e4a4a918e7d4d5317d
Oleksandra2020 <oleksandrahutor@gmail.com>
2022-06-05 19:39:33 +0300

    Added baseline Postgres for patients

---
+++ patients_service/create_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE patients (patient_name TEXT, patient_surname TEXT, patient_id INT, status TEXT, assigned_doctor_id INT, age INT, sex TEXT, diagnosis TEXT, registration_date TIMESTAMP, PRIMARY KEY(patient_id));
+
+
+-- INSERT INTO patients VALUES (1, 'Joe', 'J');
--- patients_service/postgres_client.py
+++ patients_service/postgres_client.py
@@ -1,1 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+
+def get_engine():
+    url = "postgresql://postgres:postgres@localhost:5432/"
+    engine = create_engine(url, pool_size=50, echo=False)
+
+    return engine
+
+
+def get_session(engine):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+if __name__ == '__main__':
+    engine = get_engine()
+    print(engine.url.database)
+    session = get_session(engine)
+    q_res = session.execute(f"""
+    SELECT * FROM patients WHERE patient_id = 1;
+    """)
+    for row in q_res:
+        print(row)
