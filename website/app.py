b42e0c524cdefa194fabaa4d172f865c9e304688
Daria Omelkina <odasha2001@gmail.com>
2022-06-06 01:03:22 +0300

    Foxed two pages

--- website/app.py
+++ website/app.py
@@ -5,30 +5,30 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 
 @app.route("/register", methods=["GET","POST"])
-def register():
+def register_page():
     # TODO:
     return render_template('register.html')
 
 
 @app.route("/login")
-def login():
+def login_page():
     return render_template('login.html')
 
 
-@app.route("/home")
-def home():
+@app.route("/")
+def home_page():
     # Search patients: GET (name, surname) (id) -> data
     # Search doctors: GET (name, surname) (id) -> data
     return ""
 
 
 @app.route("/patients")
-def patients():
+def patients_page():
     return ""
 
 
 @app.route("/doctors")
-def doctors():
+def doctors_page():
     return ""
 
 
