1db32890b8d934c283fe2691c54c6254c9bf7069
Oleksandra2020 <oleksandrahutor@gmail.com>
2022-06-06 13:24:03 +0300

    Added patients microservice

--- patients_service/patients_service.py
+++ patients_service/patients_service.py
@@ -1,49 +1,69 @@
 from flask_restful import reqparse, Resource, Api
 from flask import Flask
-from cassandra_client import CassandraClient
+from postgres_client import get_engine, get_session, query_by_id, query_by_name
+import json
 
 app = Flask(__name__)
 api = Api(app)
 app.config['BUNDLE_ERRORS'] = True
 
 parser = reqparse.RequestParser()
-parser.add_argument('file_name', type=str, help='File with data')
-parser.add_argument('patient_id', type=str)
-parser.add_argument('patient_name', type=str)
-parser.add_argument('patient_surname', type=str)
+parser.add_argument('file_name', type=str,
+                    help='File with data', required=False)
+parser.add_argument('patient_id', type=int, required=False)
+parser.add_argument('patient_name', type=str, required=False)
+parser.add_argument('patient_surname', type=str, required=False)
+
+
+def jsonify_result(result):
+    res = {"patients": []}
+    for i in result:
+        dct = {}
+        dct['name'] = i[0]
+        dct['surname'] = i[1]
+        dct['pattient_id'] = i[2]
+        dct['status'] = i[3]
+        dct['assigned_doctor_id'] = i[4]
+        dct['age'] = i[5]
+        dct['sex'] = i[6]
+        dct['diagnosis'] = i[7]
+        dct['registration_date'] = i[8].strftime("%m/%d/%Y, %H:%M:%S")
+
+        res["patients"].append(dct)
+
+    return res
 
 
 class RetrieveData(Resource):
-    def get():
+    def get(self):
         args = parser.parse_args()
-        table = 'patients'
-        client.connect()
-        if 'patient_id' in args:
-            result = client.read_by_id(args, table)
-            client.close()
-            return result
-        if 'patient_name' in args and 'patient_surname' in args:
-            result = client.read_by_id(args, table)
-            client.close()
-            return result
+        print(args)
+        if args['patient_id'] is not None:
+            result = query_by_id(session, args.patient_id)
+            return json.dumps(jsonify_result(result))
+        elif args['patient_name'] is not None and args['patient_surname'] is not None:
+            result = query_by_name(
+                session, args.patient_name, args.patient_surname)
+            return json.dumps(jsonify_result(result))
         else:
             return "Invalid request"
 
-    def post():
-        args = parser.parse_args()
-        table = 'patients'
-        client.connect()
-        file_name = args['file_name']
-        client.insert_record(table, None)
+    # def post(self):
+    #     args = parser.parse_args()
+    #     table = 'patients'
+    #     file_name = args['file_name']
 
-        client.close()
 
+api.add_resource(RetrieveData, '/')
 
 if __name__ == "__main__":
-    host = 'localhost'
-    port = 9042
-    keyspace = 'patients_info'
+    engine = get_engine()
+    session = get_session(engine)
+    # print(engine.url.database)
+    # q_res = session.execute(f"""
+    # SELECT * FROM patients WHERE patient_id = 1;
+    # """)
+    # for row in q_res:
+    #     print(row)
 
-    client = CassandraClient(host, port, keyspace)
-
-    app.run()
+    app.run(host='localhost', debug=True, port=8880)
--- patients_service/postgres_client.py
+++ patients_service/postgres_client.py
@@ -15,6 +15,27 @@ def get_session(engine):
     return session
 
 
+def query_by_id(session, patient_id):
+    response = session.execute(f"""
+    SELECT * FROM patients WHERE patient_id = {patient_id};
+    """)
+    return response
+
+
+def query_by_name(session, patient_name, patient_surname):
+    response = session.execute(f"""
+    SELECT * FROM patients WHERE patient_name = {patient_name} AND patient_surname = {patient_surname};
+    """)
+    return response
+
+
+def insert_data(session, data):
+    response = session.execute(f"""
+    INSERT INTO patients VALUES ();
+    """)
+    return response
+
+
 if __name__ == '__main__':
     engine = get_engine()
     print(engine.url.database)
