d4a5352e1c3062d505b7b702b32bda4f1a264717
romanyshyn-natalia <romanyshyn.n@ucu.edu.ua>
2022-06-08 12:48:42 +0300

    added docker for register service with mongodb

---
+++ init-db.js
@@ -0,0 +1,18 @@
+db = db.getSiblingDB("users");
+db['registered-users'].drop();
+
+db['registered-users'].insertMany([
+    {
+        "username": "Daria_Omelkina",
+        "email": "omelkina.n@ucu.edu.ua",
+	"role": "Main Doctor",
+        "password": "f7b39419b3f0dde181ec395d002e5090a45143740091ce3b6fa00d868e143b87"
+    },
+    {
+        "username": "Nataliia_Romanyshyn",
+        "email": "romanyshyn.n@ucu.edu.ua",
+        "role": "Doctor",
+        "password": "2373c24605d6af11bd4c3284912857072ce10f0b56f5c4112f2ee0636ef5a65a"
+    },
+
+]);
--- login-service/login.py
+++ login-service/login.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient(port=27017)
+client = pymongo.MongoClient(host="db", port=27017)
 db = client.users
 records = db['registered-users']
 
